a45a4409672198923a388fb8721f97b57fa16589
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2024-03-19 16:24:53 -0500

    All done about use reducer

--- 03-hooks-app/src/07-UseReducer/TodoApp.jsx
+++ 03-hooks-app/src/07-UseReducer/TodoApp.jsx
@@ -1,60 +1,19 @@
-import { useEffect, useReducer } from "react"
-import { todoReducerHook, TodoAdd, TodoList } from "./";
 
-const initialState = [
-  // {
-  //   id: new Date().getTime(),
-  //   description: 'Collect the soul stone',
-  //   done: false
-  // },
-  
-];
 
-//* Here we are set the function that often is used to start our reducer if we don't give a value in our initial state,
-//* Then we're parced the todos object that was set in to local storage, because the local storage only can record strings, so if we shipment an objet we obtain a [objectObjet].  
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos')) || [];
-}
+import { useTodo } from "../hooks";
+import {TodoAdd, TodoList } from "./";
 
 
 export const TodoApp = () => {
 
-   const [todos, dispatch] = useReducer(todoReducerHook, initialState, init) //! Warning: Not run the reducer, only pass it as a reference.
-
-  //* To record the todos in the local storage.
-   useEffect(() => {
-     localStorage.setItem('todos', JSON.stringify(todos))
-   }, [todos])
-   
-
-   const handleNewTodo = ( todo ) => {
-    const action = {
-      type: '[TODO] add todo',
-      payload: todo
-    }
-    dispatch( action );
-   }
-
-
-   const handleDeleteTodo = (id) => {
-     dispatch({
-      type: '[TODO] delete todo',
-      payload: id
-     })
-   }
-
-   const handleToggleTodo = (id) => {
-     dispatch({
-      type: '[TODO] toggle todo',
-      payload: id
-     })
-   }
+   const {todos, todosCount, pendingTodos, handleNewTodo, handleToggleTodo, handleDeleteTodo } = useTodo();
 
 
   return (
     <>
 
     <h1>Reducer Hook</h1>
+    <h3>Total todos: {todosCount} </h3> <small>Pending: {pendingTodos}</small>
     <hr />
 
     <div className="row">
--- 03-hooks-app/src/hooks/useTodo.js
+++ 03-hooks-app/src/hooks/useTodo.js
@@ -1,7 +1,58 @@
+import { useEffect, useReducer } from "react";
+import { todoReducerHook } from "../07-UseReducer";
+
+
+
 
 
 export const useTodo = () => {
 
-  
-  return 
+//* Here we are set the function that often is used to start our reducer if we don't give a value in our initial state,
+//* Then we're parced the todos object that was set in to local storage, because the local storage only can record strings, so if we shipment an objet we obtain a [objectObjet].  
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
 }
+
+const [todos, dispatch] = useReducer(todoReducerHook, [], init) //! Warning: Not run the reducer, only pass it as a reference.
+
+
+//* To record the todos in the local storage.
+useEffect(() => {
+  localStorage.setItem('todos', JSON.stringify(todos))
+}, [todos])
+  
+const handleNewTodo = ( todo ) => {
+  const action = {
+    type: '[TODO] add todo',
+    payload: todo
+  }
+  dispatch( action );
+ }
+
+
+ const handleDeleteTodo = (id) => {
+   dispatch({
+    type: '[TODO] delete todo',
+    payload: id
+   })
+ }
+
+ const handleToggleTodo = (id) => {
+   dispatch({
+    type: '[TODO] toggle todo',
+    payload: id
+   })
+ }
+
+ return {
+  todos,
+  todosCount: todos.length,
+  pendingTodos: todos.filter((todo) => !todo.done ).length,
+  handleNewTodo,
+  handleToggleTodo,
+  handleDeleteTodo
+ }
+
+}
+
+
