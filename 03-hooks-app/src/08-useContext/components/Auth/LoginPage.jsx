d8a1e7b6eb9bd886a2888b3cb8123b3922ab8bd2
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2024-03-23 08:58:45 -0500

    all done till useContext hook without summary

--- 03-hooks-app/src/08-useContext/components/Auth/LoginPage.jsx
+++ 03-hooks-app/src/08-useContext/components/Auth/LoginPage.jsx
@@ -1,10 +1,27 @@
+import { useContext } from "react"
+import { UserContext } from "../../context/UserContext";
 
 
 export const LoginPage = () => {
+
+   const {user, setUser} = useContext( UserContext );
+   
+
   return (
     <>
 
       <h1>LoginPage</h1>
+      
+
+      <pre> {JSON.stringify(user, null, 2)} </pre>
+
+
+      <button 
+      className="btn btn-primary"
+      onClick={() => setUser({id:2, name: 'Leydi Parra', email: 'Leydi.parra@ejemplo.com'})}
+      >
+        set user
+      </button>
       
     </>
   )
--- 03-hooks-app/src/08-useContext/context/UserProvider.jsx
+++ 03-hooks-app/src/08-useContext/context/UserProvider.jsx
@@ -1,16 +1,15 @@
 import PropTypes from 'prop-types'
 import { UserContext } from "./UserContext"
+import { useState } from 'react'
 
-const user = {
-  id: 1,
-  name: 'Diego Rodriguez',
-  email: 'diferoca@google.com'
-}
 
 //* this is a HOC because it's receiving the property called childern
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser ] = useState()
+
   return (
-    <UserContext.Provider value={{Hola: 'Mundo', user: user}}>
+    <UserContext.Provider value={{user, setUser}}>
         {children}
     </UserContext.Provider>
   )
