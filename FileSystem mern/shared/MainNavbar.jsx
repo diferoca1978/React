71eb2a2fbf77689df83a5873466a9d6c8b7b4fee
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2024-05-14 11:31:52 -0500

    filesystem was deleted

--- FileSystem mern/shared/MainNavbar.jsx
+++
@@ -1,3 +0,0 @@
-export const MainNavbar = () => {
-  return <div>MainNavBar</div>;
-};
--- FileSystem mern/shared/index.js
+++
@@ -1,4 +0,0 @@
-export * from './AuthNavbar'
-export * from './MainNavbar'
-export * from './Footer'
-export * from './ErrorPage'
