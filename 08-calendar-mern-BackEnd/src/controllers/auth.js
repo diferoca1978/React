bafdf2ed04753c52875957c38b4befa693691416
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2024-05-23 21:07:41 -0500

    JWT generated

--- 08-calendar-mern-BackEnd/src/controllers/auth.js
+++ 08-calendar-mern-BackEnd/src/controllers/auth.js
@@ -3,25 +3,92 @@
 // 2. Indicate to the response that the type is = express.response.
 
 const { response } = require('express');
-
+const bcrypt = require('bcryptjs');
 const User = require('../database/models/userModel');
-
-const loginUser = (req, res = response) => {
-  res.json({
-    ok: true,
-    user: req.body,
-  });
-};
+const { generateJWT } = require('../helpers/jwt');
 
 const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
   try {
-    const user = new User(req.body);
+    let userRegistered = await User.findOne({ email });
+
+    if (userRegistered) {
+      return res.status(400).json({
+        ok: false,
+        errors: {
+          email: {
+            msg: 'Email already in use',
+          },
+        },
+      });
+    }
+
+    user = new User(req.body);
+
+    user.password = bcrypt.hashSync(password, 5);
 
     await user.save();
 
+    // GenerateJWT
+
+    const token = await generateJWT(user.id, user.name);
+
     res.status(201).json({
       ok: true,
-      user: user,
+      user: {
+        userId: user.id,
+        name: user.name,
+        token,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Contact with customer service',
+    });
+  }
+};
+
+const loginUser = async (req, res = response) => {
+  const { email, password } = req.body;
+  console.log(typeof password);
+
+  try {
+    const userToLogin = await User.findOne({ email });
+
+    if (!userToLogin) {
+      return res.status(400).json({
+        ok: false,
+        errors: {
+          email: {
+            msg: 'User not exists',
+          },
+        },
+      });
+    }
+
+    const validPass = bcrypt.compareSync(password, userToLogin.password);
+    console.log(validPass);
+
+    if (!validPass) {
+      return res.status(400).json({
+        ok: false,
+        errors: {
+          password: {
+            msg: 'Invalid credentials',
+          },
+        },
+      });
+    }
+
+    res.json({
+      ok: true,
+      user: {
+        userId: userToLogin.id,
+        name: userToLogin.name,
+      },
     });
   } catch (error) {
     console.log(error);
